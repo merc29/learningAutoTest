c1bef70687cb6856723dd59b63d5e597c8c94b6c
Dmitrii <71825447+merc29@users.noreply.github.com>
2023-02-28 18:46:19 +0300

    Update login_as_customer.spec.js

--- cypress/integration/xyz_bank/tests/login_as_customer.spec.js
+++ cypress/integration/xyz_bank/tests/login_as_customer.spec.js
@@ -3,16 +3,31 @@ import CustomerPage from "../pageObject/loginCustomerPage";
 
 const cp = new CustomerPage();
 
+describe('Checking currency on any cutomer page', function(){ 
 Cypress.on('uncought:exceptions', () => false);
-it("view pre-customer page", () => {
-   cp.start_page();
+before( "view customer page", () => {
+   cp.login_as_any_customer()
+   /*cp.start_page();
    cp.go_to_pre_customer_page();
    
    cp.select_random_customer();
-   cp.login_customer();
+   cp.login_customer(); */
+ })
+it("checking currency", () => {
+   
    //checking currency
    cy.get('#accountSelect').select(0)
-   cy.get('.borderM > :nth-child(3)').should('eq', 'Dollar')
+   cy.get('.borderM > :nth-child(3) > :nth-child(3)').should('exist')
+   cy.get('.borderM > :nth-child(3) > :nth-child(3)').contains('Dollar')
+
+   cy.get('#accountSelect').select(1)
+   cy.get('.borderM > :nth-child(3) > :nth-child(3)').contains('Pound')
+
+   cy.get('#accountSelect').select(2)
+   cy.get('.borderM > :nth-child(3) > :nth-child(3)').contains('Rupee')
+
+
    
 
+})
 })
